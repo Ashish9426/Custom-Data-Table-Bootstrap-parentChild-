fd9c3b76f9b94a50c437f54c932a27bb2b97413a
Ashish Mishra <ashishmishra9426@gmail.com>
2021-12-16 11:03:25 +0530

    customDataTable

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,24 +16,25 @@ export class HomeComponent implements OnInit {
   iconsParent = [faTrash, faEdit, faEye]
 
   colHeaderParent = [
-                    {col:"EmpId", color:"bg-success",type:'icon', icons:this.iconsParent},
-                    {col:"Name", color:"bg-primary", type:'icon', icons:this.iconsParent},
-                    {col:"Age", color:"bg-secondary", type:'icon', icons:this.iconsParent},
-                    {col:"Salary", color:"bg-info", type:'icon', icons:this.iconsParent},
-                    {col:"Action", color:"bg-warning", type:'icon', icons:this.iconsParent}
-                  ];
-  employeeParent = [
-    {id:1, name:"Ashish", salary:15000, age:21},
-    {id:2, name:"Amit", salary:23000, age:23},
-    {id:3, name:"Manish", salary:35000, age:35},
-    {id:4, name:"Sonal", salary:45000, age:42},
-    {id:5, name:"Kritika", salary:85000, age:25},
-    {id:6, name:"Ashish", salary:27000, age:26},
-    {id:7, name:"Amit", salary:23000, age:29},
-    {id:8, name:"Manish", salary:40000, age:32},
-    {id:9, name:"Sonal", salary:45000, age:30},
-    {id:10, name:"Kritika", salary:85000, age:29},
+    {col:"EmpId", color:"bg-success text-light",type:'text', icons:this.iconsParent},
+    {col:"Name", color:"bg-primary text-danger", type:'text', icons:this.iconsParent},
+    {col:"Age", color:"bg-secondary text-light", type:'input', icons:this.iconsParent},
+    {col:"Salary", color:"bg-info text-dark", type:'text', icons:this.iconsParent},
+    {col:"Action", color:"bg-warning text-danger", type:'icon', icons:this.iconsParent}
   ];
+
+employeeParent = [
+{id:1, name:"Ashish", salary:15000, age:21, action:""},
+{id:2, name:"Shiva", salary:23000, age:23, action:""},
+{id:3, name:"Anu", salary:35000, age:35, action:""},
+{id:4, name:"Shubh", salary:45000, age:42, action:""},
+{id:5, name:"Kritika", salary:85000, age:25, action:""},
+{id:6, name:"Eva", salary:27000, age:26, action:""},
+{id:7, name:"Amit", salary:23000, age:29, action:""},
+{id:8, name:"Manish", salary:40000, age:32, action:""},
+{id:9, name:"Sonal", salary:45000, age:30, action:""},
+{id:10, name:"Kritika", salary:85000, age:29, action:""},
+];
 
   constructor() { }
 
